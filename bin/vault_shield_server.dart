4bf57cbce8018e979f33b8b4621003dceddb05b6
ch1nru5t <chinrust@gmail.com>
2024-05-01 20:16:04 +0530

    try catch

--- bin/vault_shield_server.dart
+++ bin/vault_shield_server.dart
@@ -5,13 +5,17 @@ import 'package:vault_shield_server/db.dart';
 import 'package:vault_shield_server/service.dart';
 
 void main(List<String> arguments) async {
-  await init();
-  await db?.open();
-  const PORT = 8080;
-  final service = Service();
-  var server = await HttpServer.bind('localhost', PORT);
-  shelf_io.serveRequests(server, service.handler);
-  print(
-    'Server running at http://${server.address.host}:$PORT',
-  );
+  try {
+    await init();
+    await db?.open();
+    const PORT = 8080;
+    final service = Service();
+    var server = await HttpServer.bind('localhost', PORT);
+    shelf_io.serveRequests(server, service.handler);
+    print(
+      'Server running at http://${server.address.host}:$PORT',
+    );
+  } catch (e) {
+    print(e);
+  }
 }
